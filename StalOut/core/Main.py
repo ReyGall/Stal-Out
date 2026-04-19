07be0d63e2ac7a6b51c7c78b59d2e0900a4a0ab1
Rey <romangaltsev.rey@gmail.com>
2026-04-19 19:51:45 +0300

    Build 0.0.2 save system

--- StalOut/core/Main.py
+++ StalOut/core/Main.py
@@ -1,17 +1,46 @@
 import random
 import time
+import json
 import sqlite3
 from pathlib import Path
 
 P = Path(__file__).parent
 data_F = P.parent/"data"
 db_path = data_F / "database.db"
+save_path = data_F / "save.json"
 
 con = sqlite3.connect(db_path)
 c = con.cursor()
+
+def load_game():
+     
+            global inventory
+
+            with open(save_path, "r", encoding="utf-8") as file:
+                data = json.load(file)
+                inventory = data["player"]["inventory"]
+
+load_game()
+
+def save_game():
+            
+            global inventory
+
+            data = {
+                "player": {
+                    "location": 1,
+                    "inventory": inventory
+                }
+            }
+            with open(save_path, "w") as file:
+                json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+
 current_location = 1
-temp_loot = []
-inventory = []
+debug_mode = False
+
+
 
 while True:
     print ("Enter command")
@@ -27,7 +56,12 @@ while True:
         death_chance_inc = c.fetchone() 
         death_chance = death_chance_inc [0] * 0.01
 
-        for i in range(int(raid_time)):
+        for i in range(raid_time_int):
+            if not debug_mode:
+                for second in range(60, 0, -10):
+                    print(f"Time left: {second}s...")
+                    time.sleep(10)
+
             c.execute ("SELECT * FROM loot WHERE location_id = ?", (current_location,))
             loot_list = c.fetchall()
             for item in loot_list:
@@ -36,10 +70,13 @@ while True:
                     temp_loot.append(item[0])
                     print(f"You found: {item[0]}")
 
-            player_death = random.random() < death_chance
-        if player_death == False:
+            player_alive = random.random() > death_chance
+        if player_alive == True:
             print ("Raid Success")
             inventory.extend(temp_loot)
+            temp_loot = []
+            save_game()
+
         else:
             print("You Died")
             temp_loot = []
--- StalOut/core/admin.py
+++ StalOut/core/admin.py
@@ -12,7 +12,8 @@ c = con.cursor()
 c.execute ('''CREATE TABLE IF NOT EXISTS locations(
             name text,
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            death_chance INTEGER)
+            death_chance INTEGER,
+            armor_lvl INTEGER)
             ''')
 
 c.execute ('''CREATE TABLE IF NOT EXISTS loot(
@@ -21,7 +22,16 @@ c.execute ('''CREATE TABLE IF NOT EXISTS loot(
             location_id INTEGER,
             price INTEGER,
             rarity text,
-           chance INTEGER)
+            chance INTEGER)
+            ''')
+
+c.execute ('''CREATE TABLE IF NOT EXISTS armor(
+            name text,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            class text,
+            price INTEGER,
+            rarity text,
+            lvl INTEGER)
             ''')
 
 while True:
@@ -35,10 +45,48 @@ while True:
         location_name = input()
         print("enter death chance")
         location_death_chance = input()
+        print("enter min armor lvl")
+        armor_lvl = input()
+
         c.execute (
-            "INSERT INTO locations (name,death_chance) VALUES (?,?)",
-            (location_name, location_death_chance)
+            "INSERT INTO locations (name,death_chance, armor_lvl) VALUES (?,?,?)",
+            (location_name, location_death_chance, armor_lvl)
             )
+
+    elif "edit location" in user_input:
+        print ("enter location id")
+        edit_loc_id = input()
+
+        c.execute("SELECT * FROM locations WHERE id = ?", (edit_loc_id,))
+        loc = c.fetchone()
+        
+        if loc:
+            print(f"Editing location: {loc[0]} (ID: {loc[1]})")
+            print("What do you want to change? (name, death, armor)")
+            field = input().lower().strip()
+            
+            if field == "name":
+                new_val = input("Enter new name: ")
+                c.execute("UPDATE locations SET name = ? WHERE id = ?", (new_val, edit_loc_id))
+            
+            elif field == "death":
+                new_val = input("Enter new death chance: ")
+                c.execute("UPDATE locations SET death_chance = ? WHERE id = ?", (new_val, edit_loc_id))
+            
+            elif field == "armor":
+                new_val = input("Enter new armor level: ")
+                c.execute("UPDATE locations SET armor_lvl = ? WHERE id = ?", (new_val, edit_loc_id))
+
+    elif "delete location" in user_input:
+        print ("enter location id")
+        delete_location_id = input()
+        print("are you sure? y/n")
+        loot_accept = input()
+        if loot_accept == "y":
+            c.execute("DELETE FROM locations WHERE id = ?",
+            (delete_location_id,))
+        else:
+            print("delete cancelled")
 
     elif "add loot" in user_input:
         print ("add loot name")
@@ -55,19 +103,41 @@ while True:
             "INSERT INTO loot (name, location_id, price, rarity, chance) VALUES (?,?,?,?,?)",
             (loot_name,location_id, loot_price, loot_rarity, loot_chance)
             )
-
-    elif "locations list" in user_input:
-        for location_list in c.execute ("SELECT * FROM locations ORDER BY id"):
-            print (f"name: {location_list[0]}\nid: {location_list[1]}\ndeath chance: {location_list[2]} ")
-
-
-    elif "loot list" in user_input:
-        for loot_list in c.execute ("SELECT * FROM loot ORDER BY id"):
-            print (f"name: {loot_list[0]}\n id: {loot_list[1]}\n location_id: {loot_list[2]}\n price: {loot_list[3]}\n rarity: {loot_list[4]}\n chance: {loot_list[5]}")
-
-    elif "help" in user_input:
-        print ("'add location' to add new location \n 'add loot' to add new loot \n 'locations list' to see list of locations \n 'loot list' to see list of loot")
         
+    elif "edit loot" in user_input:
+        print("enter loot id")
+        edit_loot_id = input()
+
+        c.execute("SELECT * FROM loot WHERE id = ?", (edit_loot_id,))
+        loot_item = c.fetchone()
+        
+        if loot_item:
+            print(f"Editing loot: {loot_item[0]} (ID: {loot_item[1]})")
+            print("What do you want to change? (name, location, price, rarity, chance)")
+            field = input().lower().strip()
+            
+            if field == "name":
+                new_val = input("Enter new name: ")
+                c.execute("UPDATE loot SET name = ? WHERE id = ?", (new_val, edit_loot_id))
+            
+            elif field == "location":
+                new_val = input("Enter new location ID: ")
+                c.execute("UPDATE loot SET location_id = ? WHERE id = ?", (new_val, edit_loot_id))
+            
+            elif field == "price":
+                new_val = input("Enter new price: ")
+                c.execute("UPDATE loot SET price = ? WHERE id = ?", (new_val, edit_loot_id))
+
+            elif field == "rarity":
+                new_val = input("Enter new rarity: ")
+                c.execute("UPDATE loot SET rarity = ? WHERE id = ?", (new_val, edit_loot_id))
+
+            elif field == "chance":
+                new_val = input("Enter new drop chance (1-100): ")
+                c.execute("UPDATE loot SET chance = ? WHERE id = ?", (new_val, edit_loot_id))
+            
+            con.commit()
+
     elif "delete loot" in user_input:
         print ("enter loot id")
         delete_loot_id = input()
@@ -79,16 +149,17 @@ while True:
         else:
             print("delete cancelled")
 
-    elif "delete location" in user_input:
-        print ("enter location id")
-        delete_location_id = input()
-        print("are you sure? y/n")
-        loot_accept = input()
-        if loot_accept == "y":
-            c.execute("DELETE FROM locations WHERE id = ?",
-            (delete_location_id,))
-        else:
-            print("delete cancelled")
+    elif "locations list" in user_input:
+        for location_list in c.execute ("SELECT * FROM locations ORDER BY id"):
+            print (f"name: {location_list[0]}\nid: {location_list[1]}\ndeath chance: {location_list[2]}\nmin armor lvl: {location_list[3]} ")
+
+
+    elif "loot list" in user_input:
+        for loot_list in c.execute ("SELECT * FROM loot ORDER BY id"):
+            print (f"name: {loot_list[0]}\n id: {loot_list[1]}\n location_id: {loot_list[2]}\n price: {loot_list[3]}\n rarity: {loot_list[4]}\n chance: {loot_list[5]}")
+
+    elif "help" in user_input:
+        print ("'add location' to add new location \n 'add loot' to add new loot \n 'locations list' to see list of locations \n 'loot list' to see list of loot")
 
     con.commit()
 con.close()
