fec55be2ab84f292041392a036fcdb669e2e6d0f
Rey <romangaltsev.rey@gmail.com>
2026-04-20 17:11:35 +0300

    Builds 0.0.3 Fence system

--- StalOut/core/Main.py
+++ StalOut/core/Main.py
@@ -1,8 +1,10 @@
-import random
-import time
-import json
 import sqlite3
 from pathlib import Path
+from Raid import start_raid
+from inventory import show_inventory
+from save import load_game
+from save import save_game
+from Merchants.fence import fence_merchant
 
 P = Path(__file__).parent
 data_F = P.parent/"data"
@@ -12,33 +14,13 @@ save_path = data_F / "save.json"
 con = sqlite3.connect(db_path)
 c = con.cursor()
 
-def load_game():
-     
-            global inventory
 
-            with open(save_path, "r", encoding="utf-8") as file:
-                data = json.load(file)
-                inventory = data["player"]["inventory"]
 
-load_game()
-
-def save_game():
-            
-            global inventory
-
-            data = {
-                "player": {
-                    "location": 1,
-                    "inventory": inventory
-                }
-            }
-            with open(save_path, "w") as file:
-                json.dump(data, file, indent=4, ensure_ascii=False)
-
+inventory, money = load_game(save_path,)
 
 
 current_location = 1
-debug_mode = False
+debug_mode = True
 
 
 
@@ -47,49 +29,13 @@ while True:
     user_input = input().strip().lower()
 
     if "start raid" in user_input:
-        print("enter raid time in minutes from 1 to 360")
-        raid_time = input()
-        print("Raid Started")
-        temp_loot = []
-        raid_time_int = int(raid_time)
-        c.execute ("SELECT death_chance FROM locations WHERE id = ?", (current_location,))
-        death_chance_inc = c.fetchone() 
-        death_chance = death_chance_inc [0] * 0.01
+        inventory = start_raid(inventory, current_location, debug_mode, c, save_path)
 
-        for i in range(raid_time_int):
-            if not debug_mode:
-                for second in range(60, 0, -10):
-                    print(f"Time left: {second}s...")
-                    time.sleep(10)
+    elif "inventory" in user_input:
+        show_inventory(inventory, c, money)
 
-            c.execute ("SELECT * FROM loot WHERE location_id = ?", (current_location,))
-            loot_list = c.fetchall()
-            for item in loot_list:
-                chance = item [5] / 100
-                if random.random() < chance:
-                    temp_loot.append(item[0])
-                    print(f"You found: {item[0]}")
-
-            player_alive = random.random() > death_chance
-        if player_alive == True:
-            print ("Raid Success")
-            inventory.extend(temp_loot)
-            temp_loot = []
-            save_game()
-
-        else:
-            print("You Died")
-            temp_loot = []
+    elif "fence" in user_input:
+        inventory, earned_money = fence_merchant(c, inventory)
+        money += earned_money
+        save_game(save_path, inventory, money)
         
-    item_count = {}
-
-    for item in inventory:
-        if item in item_count:
-            item_count[item]+= 1
-        else:
-            item_count[item] = 1
-
-    if "inventory" in user_input:
-        for item, count in item_count.items():
-            print(f"{item} x{count}")
-
---
+++ StalOut/core/inventory.py
@@ -0,0 +1,36 @@
+def show_inventory(inventory, c, money):
+        
+        colors = {
+             
+            "Picklock": "\033[37m",
+            "end":"\033[0m"
+
+            }
+        
+        item_count = {}
+
+        for item in inventory:
+            if item in item_count:
+                item_count[item] += 1
+            else:
+                item_count[item] = 1
+
+
+        items = c.fetchall()
+
+        print ("Your Inventory")
+        print (f"{money} Rubbles")
+
+        for item, count in item_count.items():
+
+            c.execute ("SELECT rarity FROM loot WHERE name = ?", (item,))
+            item_rarity = c.fetchone()
+            rarity_from_db = item_rarity[0].strip()
+            if rarity_from_db == "Picklock":
+                item_color = colors["Picklock"]
+
+                
+            
+            end_color = colors["end"]
+
+            print(f"{item_color}{item}{end_color} x{count}")
