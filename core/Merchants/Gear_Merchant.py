25661f9e53d53ce66fea98e729bbc3606e12c04a
Roman <romangaltsev.rey@gmail.com>
2026-04-26 18:49:46 +0300

    fix: bug fixes

--- core/Merchants/Gear_Merchant.py
+++ core/Merchants/Gear_Merchant.py
@@ -17,46 +17,48 @@ def buy_Armor(c, inventory, money, owned_armor):
 
     show_inventory(inventory, c, money)
     print ("choose which you want to buy by sending number\n")
-    choose = int(input())
-    armor_id = armor_list[choose -1][0]
+    choice = input()
+    if choice.isdigit():
+            
+        armor_id = armor_list[choice -1][0]
 
-    c.execute ("SELECT name, price FROM armor WHERE id = ?", (armor_id,))
-    result = c.fetchone()
+        c.execute ("SELECT name, price FROM armor WHERE id = ?", (armor_id,))
+        result = c.fetchone()
 
-    if result == None:
-        print ("Yo dude I have no this armor right now, come back later")
-        return inventory, money
+        if result == None:
+            print ("Yo dude I have no this armor right now, come back later")
+            return inventory, money
 
-    c.execute ("SELECT item_name, quantity FROM armor_recipes WHERE armor_id = ?", (armor_id,))
-    resourses_list = c.fetchall()
+        c.execute ("SELECT item_name, quantity FROM armor_recipes WHERE armor_id = ?", (armor_id,))
+        resourses_list = c.fetchall()
 
-    armor_name, armor_price = result
+        armor_name, armor_price = result
 
-    recipe_text = ""
+        recipe_text = ""
 
-    for req_name, req_qty in resourses_list:
+        for req_name, req_qty in resourses_list:
 
-        current_count = inventory.count(req_name)
+            current_count = inventory.count(req_name)
+            
+            recipe_text += f"{req_name} x{req_qty}\n"
+            if current_count < req_qty:
+                can_afford_resources = False
+            
+        print(f"To buy this you need:\n{armor_price} rubbles\n{recipe_text}")
+
+        if money >= armor_price and can_afford_resources == True:
+
+            money -=armor_price
+
+            for name, qty in resourses_list:
+                for _ in range(qty):
+                    inventory.remove(name)
+
+            owned_armor.append(armor_name)
+
+            print(f"successfuly bought {armor_name}")
+            return inventory, money, owned_armor
         
-        recipe_text += f"{req_name} x{req_qty}\n"
-        if current_count < req_qty:
-            can_afford_resources = False
-        
-    print(f"To buy this you need:\n{armor_price} rubbles\n{recipe_text}")
-
-    if money >= armor_price and can_afford_resources == True:
-
-        money -=armor_price
-
-        for name, qty in resourses_list:
-            for _ in range(qty):
-                inventory.remove(name)
-
-        owned_armor.append(armor_name)
-
-        print(f"successfuly bought {armor_name}")
-        return inventory, money, owned_armor
-    
-    else:
-        print ("Yo dude you have no enough money or resourses\n")
-        return inventory, money
+        else:
+            print ("Yo dude you have no enough money or resourses\n")
+            return inventory, money
--- core/Raid.py
+++ core/Raid.py
@@ -7,14 +7,22 @@ def start_raid(inventory, current_location, debug_mode, c, save_path, money):
     print("enter raid time in minutes from 1 to 360")
     try:
         raid_time = input()
+        raid_time_int = int(raid_time)
     except ValueError:
         print("invalid number")
     print("Raid Started")
     temp_loot = []
-    raid_time_int = int(raid_time)
     c.execute ("SELECT death_chance FROM locations WHERE id = ?", (current_location,))
     death_chance_inc = c.fetchone() 
+
+    if death_chance_inc is None:
+        print(f"Error: Location {current_location} not found in DB!")
+        return inventory
+
     death_chance = death_chance_inc [0] * 0.01
+
+    player_alive = True
+    temp_loot = []
 
     for i in range(raid_time_int):
         if not debug_mode:
